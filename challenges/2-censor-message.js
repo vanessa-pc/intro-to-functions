40b1b6120f2d5e40010e268ee40b870f129b69c7
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-07-27 16:57:01 +0300

    Add own test cases for challenge 2

--- challenges/2-censor-message.js
+++ challenges/2-censor-message.js
@@ -18,3 +18,9 @@ function censorMessage(message, banList){
 console.log(`censorMessage("don't mess with cats", ["mess"]):`,censorMessage("don't mess with cats", ["mess"]), `should be "don't ***** with cats"`)
 console.log(`censorMessage("Are you sure that this is safe", ["are", "is"]):`,censorMessage("Are you sure that this is safe", ["are", "is"]),`should be "***** you sure that this ***** safe"`)
 console.log(`censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"]):`,censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"]),`should be "I CANNOT ***** *****"`)
+
+
+// my test cases
+console.log(`censorMessage("Message in a bottle", ["bottle"]):`,censorMessage("Message in a bottle", ["bottle"]),`should be "Message in a *****"`)
+console.log(`censorMessage("The secret is kindness", ["kindness"]):`,censorMessage("The secret is kindness", ["kindness"]),`should be "The secret is *****"`)
+console.log(`censorMessage("Someone did it!", ["someone"]):`,censorMessage("Someone did it!", ["someone"]),`should be "***** did it!"`)
