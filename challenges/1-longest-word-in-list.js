7532948c5a86917c7027d735e2e54c48fcddb9e5
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-07-27 16:29:08 +0300

    Fix code and add test cases

--- challenges/1-longest-word-in-list.js
+++ challenges/1-longest-word-in-list.js
@@ -1,7 +1,7 @@
-function lonfestInList(arr){
+function longestInList(arr){
     let longest = arr[0]
     for (let i of arr){
-        if (i.length > longest){
+        if (i.length > longest.length){
             longest = i
         }
     }
@@ -10,9 +10,7 @@ function lonfestInList(arr){
 
 
 //test cases:
-/*
-console.log
-longestInList(["England", "Wales", "Scotland", "Northern Ireland"]) should be "Northern Ireland"
-longestInList(["the", "quick", "brown", "fox"]) should be "quick"
-longestInList(["hello", "hi", "greetings", "hey"]) should be "greetings"
-*/
+
+console.log(`longestInList(["England", "Wales", "Scotland", "Northern Ireland"]):`, longestInList(["England", "Wales", "Scotland", "Northern Ireland"]),"should be Northern Ireland")
+console.log(`longestInList(["the", "quick", "brown", "fox"]):`, longestInList(["the", "quick", "brown", "fox"]),"should be quick")
+console.log(`longestInList(["hello", "hi", "greetings", "hey"]):`, longestInList(["hello", "hi", "greetings", "hey"]),"should be greetings")
