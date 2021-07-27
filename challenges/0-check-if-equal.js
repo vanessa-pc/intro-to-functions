27e2974aeda654f8eabc25f12c500d3a1601a490
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-07-27 16:49:52 +0300

    Add own test cases

--- challenges/0-check-if-equal.js
+++ challenges/0-check-if-equal.js
@@ -13,3 +13,8 @@ console.log ("isEqual(0, 0):", isEqual(0,0), "should be true")
 console.log ("isEqual(-4, -4):", isEqual(-4,-4), "should be true")
 console.log ("isEqual(4, -4):", isEqual(4,-4), "should be false")
 console.log ("isEqual(0, 1):", isEqual(0,1), "should be false")
+
+// my test cases
+console.log ("isEqual(33.3, 1):", isEqual(0,1), "should be false")
+console.log ("isEqual(-6, -6.0):", isEqual(0,1), "should be false")
+console.log ("isEqual(2, 9):", isEqual(0,1), "should be false")
