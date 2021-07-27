d5fcb11b9bf458648211cf9990723d6fef38073e
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-07-27 16:53:28 +0300

    Add own test cases for challenge 1

--- challenges/1-longest-word-in-list.js
+++ challenges/1-longest-word-in-list.js
@@ -14,3 +14,9 @@ function longestInList(arr){
 console.log(`longestInList(["England", "Wales", "Scotland", "Northern Ireland"]):`, longestInList(["England", "Wales", "Scotland", "Northern Ireland"]),"should be Northern Ireland")
 console.log(`longestInList(["the", "quick", "brown", "fox"]):`, longestInList(["the", "quick", "brown", "fox"]),"should be quick")
 console.log(`longestInList(["hello", "hi", "greetings", "hey"]):`, longestInList(["hello", "hi", "greetings", "hey"]),"should be greetings")
+
+
+// my test cases
+console.log(`longestInList(["salutations", "goodbye", "hi", "world"]):`, longestInList(["salutations", "goodbye", "hi", "world"]),"should be salutations")
+console.log(`longestInList(["car", "bike", "aeroplane", "boat", "roller-coaster"]):`, longestInList(["car", "bike", "aeroplane", "boat", "roller-coaster"]),"should be roller-coaster")
+console.log(`longestInList(["dog", "horse", "cat", "eagle", "hyena"]):`, longestInList(["dog", "horse", "cat", "eagle", "hyena"]),"should be horse")
