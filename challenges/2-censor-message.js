649d31d8baf8422c85be0ac629b9e45deaecc36b
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-07-27 16:47:41 +0300

    Fix code and add test cases for challenge 2

--- challenges/2-censor-message.js
+++ challenges/2-censor-message.js
@@ -3,7 +3,7 @@ function censorMessage(message, banList){
     let scrubbedWords = []
     for (let i of rawWords){
         let lowercaseWord = i.toLowerCase()
-        if (banList.contains(lowercaseWord)){
+        if (banList.includes(lowercaseWord)){
             scrubbedWords.push("*****")
         } else {
             scrubbedWords.push(i)
@@ -12,3 +12,9 @@ function censorMessage(message, banList){
     let scrubbedMessage = scrubbedWords.join(" ")
     return scrubbedMessage
 }
+
+// test cases:
+
+console.log(`censorMessage("don't mess with cats", ["mess"]):`,censorMessage("don't mess with cats", ["mess"]), `should be "don't ***** with cats"`)
+console.log(`censorMessage("Are you sure that this is safe", ["are", "is"]):`,censorMessage("Are you sure that this is safe", ["are", "is"]),`should be "***** you sure that this ***** safe"`)
+console.log(`censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"]):`,censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"]),`should be "I CANNOT ***** *****"`)
